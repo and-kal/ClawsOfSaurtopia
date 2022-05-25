93286efa81de16f00cd9b0e694a17ce524d4c7db
callnasty <andre@grrlz.net>
2022-05-25 14:48:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+𝕮laws 𝕺f 𝕾aurtopia 𝕹oise 𝖋estival
+
+May 27 and May 28, 2022
+in Leipzig
+
+
+#### Fr 27.5.:
+- [St. Toupet](https://youtu.be/RSVMNu6pQw8+)
+- Icmefur
+- [Cesar Palace](https://cesarpalace.bandcamp.com)
+- [Genesis Victoria](https://youtu.be/ZpbreLDqeTA)
+- [Duflan Duflan](https://duflanduflan.bandcamp.com)
+- Aftershow: Steiner + Two Dots
+
+
+
+#### Sa 28.5.:
+- [Rusz](https://goldendoomrecords.bandcamp.com/album/hell)
+- [Catarat](https://soundcloud.com/catarat/c100000000)
+- [Eyal Talmor](https://crippleclerk.bandcamp.com/)
+- [Kurws](https://kurws.bandcamp.com)
+- [STAXL](https://and-kal.github.io/livecoding)
+- [YC-CY](https://yc-cy.bandcamp.com/)
+- Aftershow: [Valeska](https://soundcloud.com/va-les-ka) & [Gleeetch](https://soundcloud.com/gleeetch)
---
+++ src/Main.svelte
@@ -0,0 +1,256 @@
+<script>
+</script>
+
+<header>
+  <h1 hidden>Claws Of Saurtopia #8 ~ May 27 + 28, 2022</h1>
+  <img
+    id="hero"
+    src="./assets/typo-bands-min/clawsofsaurtopianoisefest8.png"
+    alt="Orange-blue poster for Claws Of Saurtopia festival 2022"
+  />
+  <div>
+    Hey dear noise heads! After a rather long hiatus, we are happy to be back
+    with another edition of the earpiercing, gutwrenching, jurassic <em
+      >Claws of Saurtopia</em
+    >
+    noise festival! Happening <em>May 27 and May 28, 2022 at ZXRX Leipzig</em>!
+  </div>
+</header>
+<main>
+  <div class="timetable-container">
+    <div class="date-head"><h2>[Friday]<br />May 27, 2022</h2></div>
+    <div>
+      <div>??:??</div>
+      <a href="https://youtu.be/RSVMNu6pQw8+">YT</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/sttoupet.min.png"
+      alt="band logo of sttoupet"
+    />
+    <div>
+      <div>??:??</div>
+      <!--<a href=""></a>-->
+    </div>
+    <img
+      src="./assets/typo-bands-min/icmefur.min.png"
+      alt="band logo of icmefur"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://cesarpalace.bandcamp.com">BC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/cesarpalace.min.png"
+      alt="band logo of cesarpalace"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://youtu.be/ZpbreLDqeTA">YT</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/genesisvictoria.min.png"
+      alt="band logo of genesisvictoria"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://duflanduflan.bandcamp.com">BC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/duflanduflan.min.png"
+      alt="band logo of duflanduflan"
+    />
+    <div>
+      <div>??:??</div>
+      <!--<a href=""></a>-->
+    </div>
+    <img
+      src="./assets/typo-bands-min/steiner+twodots.min.png"
+      alt="band logo of steiner+twodots"
+    />
+  </div>
+  <div class="poster-container">
+    <img
+      src="./assets/nf8-sieb-frei.jpg"
+      alt="orange-blue poster with cyborg dinosaurs for Claws Of Saurtopia festival 2022"
+    />
+  </div>
+  <div class="timetable-container">
+    <div class="date-head"><h2>[Saturday]<br />May 28, 2022</h2></div>
+    <div>
+      <div>??:??</div>
+      <a href="https://goldendoomrecords.bandcamp.com/album/hell">BC</a>
+    </div>
+    <img src="./assets/typo-bands-min/rusz.min.png" alt="band logo of rusz" />
+    <div>
+      <div>??:??</div>
+      <a href="https://soundcloud.com/catarat/c100000000">SC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/catarat.min.png"
+      alt="band logo of catarat"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://crippleclerk.bandcamp.com/">BC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/eyaltalmor.min.png"
+      alt="band logo of eyaltalmor"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://kurws.bandcamp.com">BC</a>
+    </div>
+    <img src="./assets/typo-bands-min/kurws.min.png" alt="band logo of kurws" />
+    <div>
+      <div>??:??</div>
+      <a href="https://and-kal.github.io/livecoding">GH</a>
+    </div>
+    <img src="./assets/typo-bands-min/staxl.min.png" alt="band logo of staxl" />
+    <div>
+      <div>??:??</div>
+      <a href="https://yc-cy.bandcamp.com/">BC</a>
+    </div>
+    <img src="./assets/typo-bands-min/yc-cy.min.png" alt="band logo of yc-cy" />
+    <div>
+      <div>??:??</div>
+      <a href="https://soundcloud.com/va-les-ka">SC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/valeska.min.png"
+      alt="band logo of valeska"
+    />
+    <div>
+      <div>??:??</div>
+      <a href="https://soundcloud.com/gleeetch">SC</a>
+    </div>
+    <img
+      src="./assets/typo-bands-min/gleeetch.min.png"
+      alt="band logo of gleeetch"
+    />
+  </div>
+</main>
+
+<footer>
+  <a href="/impressum.html">🦖 🖱Impressum 🦕</a>
+  <a href="https://clawsofsaurtopia-blog.tumblr.com">🦖 🖱Archive 🦕</a>
+</footer>
+
+<style>
+  header {
+    text-align: center;
+  }
+  header #hero {
+    max-width: 80%;
+    margin: 2rem auto;
+  }
+  header div {
+    font-size: 2rem;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: none;
+    /* height: auto; */
+    /* max-height: 100%; */
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 2fr 1fr 2fr;
+  }
+
+  a {
+    background-color: #f55732;
+    color: rgb(87, 9, 189);
+    font-size: 1.5rem;
+    text-decoration: none;
+    border: 3px solid #333;
+    padding: 0.25em;
+  }
+
+  a:hover {
+    transition: background-color 1s;
+    background-color: rgb(0, 255, 21);
+    color: rgb(87, 9, 189);
+  }
+
+  em {
+    font-weight: 700;
+    font-style: italic;
+  }
+
+  .poster-container {
+    align-self: center;
+  }
+  .poster-container img {
+    max-width: 95%;
+    box-shadow: 5px 5px #333;
+  }
+
+  .timetable-container {
+    max-width: 80%;
+    /* max-height: 60vh; */
+    margin: 2rem auto;
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: left;
+    row-gap: 2em;
+    column-gap: 1em;
+  }
+
+  .timetable-container * {
+    align-self: center;
+  }
+
+  .timetable-container .date-head {
+    grid-column: 1 / 3;
+    font-size: 1.75rem;
+    align-self: baseline;
+    max-width: 80%;
+  }
+  .timetable-container .date-head h2 {
+    background-color: #096cbd;
+    color: #f55732;
+    text-align: center;
+    border: 4px solid #333;
+    /* display: inline-block; */
+    padding: 0.33em;
+  }
+
+  .timetable-container div {
+    text-align: center;
+  }
+
+  .timetable-container img {
+    max-width: 60%;
+  }
+  footer {
+    margin-top: 2rem;
+    text-align: right;
+  }
+
+  @media (max-width: 1024px) {
+    header #hero {
+      max-width: 99%;
+      margin: 1rem auto;
+    }
+    header div {
+      font-size: 1.33rem;
+    }
+    .timetable-container .date-head {
+      font-size: 1.33rem;
+      max-width: 80%;
+    }
+    main {
+      grid-template-columns: 1fr;
+    }
+    .timetable-container {
+      max-width: 99%;
+      justify-items: center;
+    }
+    .poster-container {
+      display: none;
+    }
+  }
+</style>
