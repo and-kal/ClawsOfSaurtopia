be289016d8f07b7e01c54432c51fe2c6fac7cc1f
and-kal <andre@grrlz.net>
2023-04-12 22:01:08 +0200

    switch to webC+nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(pluginWebc, {
     components: [
       "_components/**/*.webc",
@@ -8,7 +10,7 @@ module.exports = function (eleventyConfig) {
       "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc",
     ],
   });
-  eleventyConfig.addPassthroughCopy("./_includes/*.css");
+  eleventyConfig.addPassthroughCopy("./assets/css/*.css");
   eleventyConfig.addPassthroughCopy("./assets");
   return {
     passthroughFileCopy: true,
