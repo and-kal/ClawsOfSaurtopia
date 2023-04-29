efc0e2fcf2abe78a1632967a4e03ac3438f7a42b
and-kal <andre@grrlz.net>
2023-04-29 14:53:54 +0200

    add workshops; remove webc

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,7 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPlugin(pluginWebc, {
-    components: [
-      "_components/**/*.webc",
-      "npm:@11ty/is-land/*.webc",
-      "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc",
-    ],
-  });
   eleventyConfig.addPassthroughCopy("./assets/css/*.css");
   eleventyConfig.addPassthroughCopy("./archive/*.css");
   eleventyConfig.addPassthroughCopy("./assets/fonts");
