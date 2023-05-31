97be180027fccba1b1eebb15ac14bd8f9f902d48
and-kal <andre@grrlz.net>
2023-05-31 11:40:51 +0200

    add stage times

--- .eleventy.js
+++ .eleventy.js
@@ -16,18 +16,18 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("bandsAndColours", function (roster) {
     const randomColourPairings = [
-      ["#f9e07a", "blueviolet"],
-      ["fuchsia", "#34086d"],
-      ["#B506A5", "#DEFD17"],
-      ["#631f8e", "yellow"],
-      ["#884198", "#68ECE8"],
-      ["#cdd460", "blue"],
-      ["#050f3b", "springgreen"],
-      ["#090494", "#23DA8B"],
+      ["#E89026", "#421E89"],
       ["#D632DB", "#0E091C"],
       ["#74BE2E", "#1F1119"],
-      ["#E89026", "#421E89"],
+      ["fuchsia", "#34086d"],
       ["#C66035", "#0E0C00"],
+      ["#B506A5", "#DEFD17"],
+      ["#884198", "#68ECE8"],
+      ["#090494", "#23DA8B"],
+      ["#cdd460", "blue"],
+      ["#f9e07a", "blueviolet"],
+      ["#050f3b", "springgreen"],
+      ["#631f8e", "yellow"],
     ];
 
     const bandsWarmup = [
@@ -52,80 +52,103 @@ module.exports = function (eleventyConfig) {
         link: "https://soundcloud.com/fatiguesuspecte",
       },
     ];
-    const bandsFestival = [
+    const bandsFestivalFriday = [
       {
         name: "Klöße {uk/de}",
         bio: "stumbling beats & eerie invocations",
         link: "https://kloosuh.bandcamp.com/album/kl-e",
+        time: "9:00pm-9:45pm",
       },
       {
         name: "Gleetch Noise Show {lpz}",
         bio: "games with amplified toys",
         link: "https://soundcloud.com/intergalacticatadventures",
+        time: "9:45pm-10:30pm",
       },
       {
         name: "Rundfunkorchestra {de}",
         bio: "electro-acoustic complications",
         link: "https://sonicimpulses.org/rundfunkorchester/",
+        time: "10:30pm-11:15pm",
       },
       {
         name: "Territorial Gobbing {uk}",
         bio: "performative cut-up nonsense",
         link: "https://territorialgobbing.bandcamp.com",
+        time: "11:15pm-12:00am",
       },
       {
         name: "It’s Everyone Else {slov/lpz)",
         bio: "oppressive industrial beats",
         link: "https://itseveryoneelse.bandcamp.com",
+        time: "12:00am-12:45am",
       },
       {
         name: "Gorz {bln}",
         bio: "demonic futurist r&b",
         link: "https://lara-alarcon.bandcamp.com/",
+        time: "12:45am-1:30am",
       },
       {
         name: "Nape Neck {uk}",
         bio: "punk funk chants",
         link: "https://napeneck.bandcamp.com",
+        time: "1:30am-2:15am",
       },
+    ];
+
+    const bandsFestivalSaturday = [
       {
         name: "Deeskalationskommando {lpz}",
         bio: "light-controlled experimentalism",
         link: "https://soundcloud.com/inushini/deeskalationskommoando",
+        time: "9:00pm-9:45pm",
       },
       {
         name: "Makroplastik {de}",
         bio: "glitchy broken beats",
         link: "https://makroplastik.bandcamp.com/album/a-b-r-a-z-e-n",
+        time: "9:45pm-10:30pm",
       },
       {
         name: "Sathönay {fr}",
         bio: "mediterranean psych folk",
         link: "https://sathonay.bandcamp.com",
+        time: "10:30pm-11:15pm",
       },
       {
         name: "Earth Logoff {de}",
         bio: "minimalist drum ‘n’ noise",
         link: "https://kitchenlegrecordsberlin.bandcamp.com/album/earthlogoff-fungym",
+        time: "11:15pm-12:00am",
       },
       {
         name: "Fiat X Multipla {de}",
         bio: "8bit harsh noise",
         link: "#",
+        time: "12:00am-12:45am",
       },
       {
         name: "Heimat {fr}",
         bio: "fake wave franco-allemande",
         link: "https://meineheimat.bandcamp.com/ ",
+        time: "12:45am-1:30am",
       },
       {
         name: "Atol Atol Atol {PL}",
         bio: "danceable spasmodic delight",
         link: "https://atolatolatol.bandcamp.com/album/koniec-sosu-tysi-ca-wysp",
+        time: "1:30am-2:15am",
+      },
+      {
+        name: "Fatique Suspecte {Lpz}",
+        bio: "A duo combining experimental breakcore/techno with ambient and noise undertones, equally matching for outdoor raves and the dirty basement of some diy punk squat.",
+        link: "https://soundcloud.com/fatiguesuspecte",
+        time: "2:15am-open end",
       },
     ];
 
-    const bands = roster === "warmup" ? bandsWarmup : bandsFestival;
+    const bands = roster === "warmup" ? bandsWarmup : (roster === "festivalFriday" ? bandsFestivalFriday : bandsFestivalSaturday);
 
     let htmlElement = "";
 
@@ -135,17 +158,22 @@ module.exports = function (eleventyConfig) {
 
       htmlElement += `<div
         class='band'
-        style="color:${currentColourPair[0]};background-color:${currentColourPair[1]};border:3px groove ${currentColourPair[0]}">
+        style="color:${currentColourPair[0]};background-color:${
+        currentColourPair[1]
+      };border:3px groove ${currentColourPair[0]}">
         <a href='${band.link}' 
           target='_blank' rel='noopener noreferrer' 
           class='band_link'
-          style="color:${currentColourPair[1]};background-color:${currentColourPair[0]}"
+          style="color:${currentColourPair[1]};background-color:${
+        currentColourPair[0]
+      }"
         >
           ${band.name}
         </a>
         <div class='band_bio'>
           ${band.bio}
         </div>
+        ${band.time ? `<div class="">${band.time}</div>` : ""}
       </div>
     `;
     });
