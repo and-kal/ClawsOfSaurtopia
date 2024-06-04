fbf79011b940108d98e21883e669705f1acf0e12
and-kal <andre@grrlz.net>
2024-06-04 10:50:23 +0200

    update archive

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets/css/*.css");
   eleventyConfig.addPassthroughCopy("./assets/fonts");
   eleventyConfig.addPassthroughCopy("./assets/img");
+  eleventyConfig.addPassthroughCopy("./assets/other");
   eleventyConfig.addPassthroughCopy("./assets/video");
   eleventyConfig.addPassthroughCopy("./assets/*.*");
   eleventyConfig.addPassthroughCopy("./archive/2022");
