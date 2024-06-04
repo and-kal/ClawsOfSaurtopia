4cdc241dd839d022e73d6374a4c89fc06d2cb9b6
and-kal <andre@grrlz.net>
2024-06-04 10:49:29 +0200

    update archive

--- archive/html/gettimestamps.py
+++ archive/html/gettimestamps.py
@@ -2,7 +2,7 @@ import os
 from bs4 import BeautifulSoup
 
 def read_html_files(folder_path):
-    files = os.listdir(folder_path)
+    files = sorted(os.listdir(folder_path), key=len)
     html_files = [file for file in files if file.endswith('.html')]
     
     for html_file in html_files:
@@ -14,13 +14,19 @@ def read_html_files(folder_path):
         soup = BeautifulSoup(content, 'html.parser')
         
         timestamp_element = soup.find(id='timestamp')
+        caption_elements = soup.find_all('div', {'class':'caption'})
         
         if timestamp_element:
             timestamp = timestamp_element.get_text(strip=True)
         else:
             timestamp = 'No timestamp element found'
         
-        print(f"<div class='post_link' id='{html_file.strip('.html')}'>{timestamp}: <a href='html/{html_file}' target='_blank'>{html_file.strip('.html')}</a></div>")
+        caption = 'blog post'
+        if caption_elements:
+            for caption_element in caption_elements:
+                caption = caption_element.get_text(strip=True)
+        
+        print(f"<div class='post_link' id='{html_file.strip('.html')}'><a class='underline' href='html/{html_file}' target='_blank'>{caption}</a> from {timestamp}</div>")
 
 folder_path = './'
 read_html_files(folder_path)
